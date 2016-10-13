170d5d9d43cda62e07d90221af1c81eb4ae4fa56
Jonny Dee <jonny.dee@gmx.net>
2016-10-13 21:33:31 +0200

    IonicHistory: Add optional integer argument to 'goBack()' method.

--- src/ionic/navigation/IonicHistory.hx
+++ src/ionic/navigation/IonicHistory.hx
@@ -16,7 +16,7 @@ extern class IonicHistory {
 	public function backTitle() : String;
 	public function forwardView() : Dynamic;
 	public function currentStateName() : String;
-	public function goBack() : Void;
+	public function goBack(backCount: Int = -1) : Void;
 	public function clearHistory() : Void;
 	public function clearCache() : Void;
 	public function nextViewOptions(options : ViewOptions) : Void;
